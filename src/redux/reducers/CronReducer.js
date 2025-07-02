a1c4e713351f1ee3d36836ab19576c6a70a908e5
Your Name <your_email@example.com>
2025-07-02 17:02:33 +0530

    i set univercel api  commit

--- src/redux/reducers/CronReducer.js
+++ src/redux/reducers/CronReducer.js
@@ -6,7 +6,7 @@ export const fetchCrons = createAsyncThunk(
   'Crons/getAllCronsData',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('http://localhost:3000/api/Crons/getAllCronsData/'); 
+      const response = await axios.get('https://scheduled-cron-backend.onrender.com/api/Crons/getAllCronsData/'); 
       return response.data.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response?.data?.message || "Fetch failed");
